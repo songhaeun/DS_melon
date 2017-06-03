7cdbd57e764339469fbf566790829490515a2349
옆집메론\뀨 <song96244@gmail.com>
2017-06-04 02:53:38 +0900

    책에있는거 c로바꾼거

--- redblacktree/redblacktree/rb소스1.cpp
+++ redblacktree/redblacktree/rb소스1.cpp
@@ -2,178 +2,481 @@
 #include<malloc.h>
 #define BLACK 0
 #define RED 1
-
-typedef struct RBnode {
+//BST지우고 교재있는거대로 써보기
+//BST를 없애버리고 RBNode *root=NULL로 루트만들기
+//typedef struct RBNode* RBNodePtr;
+typedef struct RBNode *RBNodePtr;
+struct RBNode{
 	int val, color;
-	RBnode *left;
-	RBnode *right;
-	RBnode *parent;
-	RBnode(int newval) {
-		val = newval;
-		left = right = parent = NULL;
-	}
-}rbnode;
-typedef struct RBtree {
-	rbnode *root;
-	void leftrotation(rbnode *&, rbnode*&);
-	void rightrotation(rbnode *&, rbnode*&);
-	void rbfixup(rbnode *&, rbnode*&);
-	RBtree() {
-		root = NULL;
-	}
-	void insert(int newval);
-	void print();
-	void inorder();
-}rbtree;
-void inorderhelper(rbnode *node) {
-	if (node == NULL)
-		return;
-	inorderhelper(node->left);
-	printf("%d[%d]", node->val, node->color);
-	inorderhelper(node->right);
+	RBNodePtr left, right, parent;
+};
+RBNodePtr rbnode_alloc(int newval) {
+RBNodePtr self = (RBNodePtr)malloc(sizeof(struct RBNode));
+	self->val = newval;
+	self->color = BLACK;
+	self->left = NULL;
+	self->right = NULL;
+	self->parent = NULL;
+	return self;
 }
-rbnode *bstinsert(rbnode*tree, rbnode *ttree) {
-	if (tree == NULL)
-		return ttree;
-	if (ttree->val < tree->val) {
-		tree->left = bstinsert(tree->left, ttree);
-		tree->left->parent = tree;
-	}
-	else if (ttree->val >tree->val) {
-		tree->right = bstinsert(tree->right, ttree);
-		tree->right->parent = tree;
-	}
-	return tree;
+void insertRBNode(RBNodePtr root, RBNodePtr newval);//데이터 삽입
+void leftrotation(RBNodePtr root, RBNodePtr newval);
+void rightrotation(RBNodePtr root, RBNodePtr newval);
+void fixup(RBNodePtr y, RBNodePtr root, RBNodePtr newval);
+void print(RBNodePtr root, int level);//출력
+//void inorder(RBNodePtr root);
+int main(void) {
+	RBNodePtr root = NULL;
+	printf("11");
+	insertRBNode(root, rbnode_alloc(10));
+	insertRBNode(root, rbnode_alloc(20));
+	insertRBNode(root, rbnode_alloc(30));
+	print(root, 0);
+	//inorder(root);
 }
-
-void leftrotation(rbnode*&tree, rbnode*&ttree) {
-	rbnode *newtree = ttree->right;
-	ttree->right = newtree->left;
-	if (ttree->right != NULL) //!=nil로 책은레프트
-		ttree->right->parent = ttree;//책레프트
-	newtree->parent = ttree->parent;
-	if (ttree->parent == NULL)
-		tree = newtree;
-	else if (ttree == ttree->parent->left)
-		ttree->parent->left = newtree;
+void insertRBNode(RBNodePtr root, RBNodePtr newval) {
+	RBNodePtr y = NULL;
+	RBNodePtr x = root;
+	while (x != NULL) {
+		y = x;
+		if (newval->val < x->val)
+			x = x->left;
+		else
+			x = x->right;
+	}
+	newval->parent = y;
+	if (y == NULL)
+		root = newval;
+	else if (newval->val < y->val)
+		y->left = newval;
 	else
-		ttree->parent->right = newtree;
-	newtree->left = ttree;
-	ttree->parent = newtree;
+		y->right = newval;
+	newval->left = NULL;
+	newval->right = NULL;
+	newval->color = RED;
+	fixup(y,root, newval);
 }
-void rightrotation(rbnode*&tree, rbnode*&ttree) {
-	rbnode*newtree = ttree->left;
-	ttree->right = newtree->left;
-	if (ttree->right != NULL)
-		ttree->right->parent = ttree;
-	newtree->parent = ttree->parent;
-	if (ttree->parent == NULL)
-		tree = newtree;
-	else if (ttree == ttree->parent->left)
-		ttree->parent->left = newtree;
-	else
-		ttree->parent->right = newtree;
-	newtree->left = ttree;
-	ttree->parent = newtree;
-}
-void rbfixup(rbnode *&tree, rbnode*&ttree) {
-	rbnode *parent = NULL;
-	rbnode *gparent = NULL;
-	while ((ttree != tree) && (ttree->color != BLACK) && (ttree->parent->color == RED)) {
-		parent = ttree->parent;
-		gparent = ttree->parent->parent;
-		if (parent == gparent->right) {
-			rbnode*uncle = gparent->right;
-			if (uncle != NULL && uncle->color == RED) {
-				gparent->color = RED;
-				parent->color = BLACK;
-				uncle->color = BLACK;
-				ttree = gparent;
+void fixup(RBNodePtr y, RBNodePtr root, RBNodePtr newval) {
+	while (newval->parent->color == RED) {
+		if (newval->parent == newval->parent->parent->left) {
+			y = newval->parent->parent->right;
+			if (y->color == RED) {
+				newval->parent->color = BLACK;
+				y->color = BLACK;
+				newval->parent->parent->color = RED;
+				newval = newval->parent->parent;
+			}
+			else if (newval == newval->parent->right) {
+				newval = newval->parent;
+				leftrotation(root, newval);
 			}
 			else {
-				if (ttree == parent->right) {
-					rightrotation(tree, parent);
-					ttree = parent;
-					parent = ttree->parent;
-				}
-				leftrotation(tree, gparent);
-				int tmp = parent->color;
-				parent->color = gparent->color;
-				gparent->color = tmp;
-				//swap(parent->color, gparent->color);
-				ttree = parent;
+				newval->parent->color = BLACK;
+				newval->parent->parent->color = RED;
+				rightrotation(root, newval->parent->parent);
 			}
 		}
 		else {
-			rbnode *uncle = gparent->left;
-			if ((uncle != NULL) && (uncle->color == RED)) {
-				gparent->color = RED;
-				parent->color = BLACK;
-				uncle->color = BLACK;
-				ttree = gparent;
+			y = newval->parent->parent->left;
+			if (y->color == RED) {
+				newval->parent->color = BLACK;
+				y->color = BLACK;
+				newval->parent->parent->color = RED;
+				newval = newval->parent->parent;			
+			}
+			else if (newval == newval->parent->left) {
+				newval = newval->parent;
+				rightrotation(root, newval);
 			}
 			else {
-				if (ttree == parent->left) {
-					rightrotation(tree, parent);
-					ttree = parent;
-					parent = ttree->parent;
+				newval->parent->color = BLACK;
+				newval->parent->parent->color = RED;
+				leftrotation(root, newval->parent->parent);
+			}
+		}
+	}
+	root->color = BLACK;
+}
+void leftrotation(RBNodePtr root, RBNodePtr newval) {
+	RBNodePtr y = newval->right;
+	newval->right = y->left;
+	if (y->left != NULL)
+		y->left->parent = newval;
+	y->parent = newval->parent;
+	if (newval->parent == NULL)
+		root = y;
+	else if (newval == newval->parent->left)
+		newval->parent->left = y;
+	else
+		newval->parent->right = y;
+	y->left = newval;
+	newval->parent = y;
+}
+void rightrotation(RBNodePtr root, RBNodePtr newval) {
+	RBNodePtr y = newval->left;
+	newval->left = y->right;
+	if (y->right != NULL)
+		y->right->parent = newval;
+	y->parent = newval->parent;
+	if (newval->parent == NULL)
+		root = y;
+	else if (newval == newval->parent->right)
+		newval->parent->right = y;
+	else
+		newval->parent->left = y;
+	y->right = newval;
+	newval->parent = y;
+}
+void print(RBNodePtr root, int level) {
+	if (root->right != NULL)
+		print(root->right, level + 1);
+	for (int i = 0; i < level; i++)
+		printf("    ");
+	printf("%d[%d]\n", root->val, root->color);
+	if (root->left != NULL)
+		print(root->left, level + 1);
+}
+
+ 
+
+
+/*void main() {
+	printf("aa");
+	BSTPtr bst = bst_alloc();
+	printf("bb");
+	rb_insert(bst, bst->root, rbnode_alloc(11));
+	rb_insert(bst, bst->root, rbnode_alloc(2));
+	rb_insert(bst, bst->root, rbnode_alloc(1));
+	rb_insert(bst, bst->root, rbnode_alloc(7));
+	rb_insert(bst, bst->root, rbnode_alloc(5));
+	rb_insert(bst, bst->root, rbnode_alloc(4));
+	rb_insert(bst, bst->root, rbnode_alloc(14));
+	rb_insert(bst, bst->root, rbnode_alloc(8));
+	rb_insert(bst, bst->root, rbnode_alloc(15));
+	printf("cc");
+	rb_print(bst, bst->root, 0);
+	printf("\n");
+	rb_inorder(bst, bst->root);
+	printf("\n");
+	printf("\n[0]-BLACK [1]-RED\n");
+	printf("\n");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*typedef struct BST* BSTPtr;
+struct BST {
+	RBNodePtr root;
+};
+
+BSTPtr bst_alloc() {
+	BSTPtr self = (BSTPtr)malloc(sizeof(struct BST));
+	self->root = NULL;
+	return self;
+}*/
+
+/*void rb_insert_fixup(/*BSTPtr self,*//* RBNodePtr tree, RBNodePtr n);*/
+/*void rb_insert(BSTPtr self, RBNodePtr tree, RBNodePtr n) {
+	/*RBNodePtr y = NULL;
+	RBNodePtr x = self->root;
+	while (x != NULL) {
+		y = x;
+		if (n->val < x->val)
+			x = x->left;
+		else
+			x = x->right;
+	}
+	n->parent = y;
+	if (y == NULL)
+		self->root = n;
+	else if (n->val < y->val)
+		y->left = n;
+	else
+		y->right = n;
+	n->left = NULL;
+	n->right = NULL;
+	n->color = RED;
+	rb_insert_fixup(tree, n);*/
+	/*RBNodePtr tmp = self->root;
+	while (tmp != NULL) {
+		self->root = tmp;
+		if (n->val < tmp->val)
+			tmp = tmp->left;
+		else
+			tmp = tmp->right;
+	}
+	n->parent = self->root;
+	if (self->root == NULL)
+		self->root = n;
+	else if (n->val < tree->val)
+		tree->left = n;
+	else
+		tree->right = n;
+	n->left = NULL;
+	n->right = NULL;
+	n->color = RED;
+	rb_insert_fixup(tree, n);
+	/*if (self->root == NULL)
+		self->root = n;
+	else if (n->val < tree->val) {
+		if (tree->left == NULL)
+		{
+			tree->left = n;
+			//tree->left->color = RED;//여기서주면 지나치게 단순해지니까 픽스업/로테이트
+		}
+		else
+			rb_insert(self, tree->left, n);
+	}
+	else {
+		if (tree->right == NULL)
+		{
+			tree->right = n;
+			//tree->right->color = RED;
+		}
+		else
+			rb_insert(self,tree->right, n);
+	}
+	//rb_insert_fixup(/*self,*///tree, n);
+	//--여기서오류/위에색지정한거 
+	//지우면 오류 안나는데 색은안들어감//로테이션에서 오류
+	//픽스업에서 오류, 로테이션 에서 셀프지우고 pt_지정하는거 하고나서 안먹으 
+//}
+/*void rb_leftrotation(RBNodePtr tree, RBNodePtr n) {
+	RBNodePtr pt_right = n->right;
+	n->right = pt_right->left;
+	if (pt_right->left != NULL)//n->right != NULL
+		pt_right->left->parent = n;//n->right->parent = n;
+	pt_right->parent = n->parent;
+	if (n->parent == NULL)
+		tree=pt_right;
+	else if (n == n->parent->left)
+		n->parent->left = pt_right;
+	else
+		n->parent->right = pt_right;
+	pt_right->left = n;
+	n->parent = pt_right;
+}
+void rb_rightrotation(RBNodePtr tree, RBNodePtr n) {
+	RBNodePtr pt_left = n->left;
+	n->left = pt_left->right;
+	if (pt_left->right != NULL)//n->left != NULL
+		pt_left->right->parent = n;//n->left->parent = n;
+	pt_left->parent = n->parent;
+	if (n->parent == NULL)
+		tree=pt_left;
+	else if (n == n->parent->right)
+		n->parent->right = pt_left;
+	else
+		n->parent->left = pt_left;
+	pt_left->right = n;
+	n->parent = pt_left;
+}
+void rb_insert_fixup(/*BSTPtr self,*///RBNodePtr tree, RBNodePtr n) {
+	/*
+	RBNodePtr y = NULL;
+	while (n->parent->color == RED) {
+		if (n->parent == n->parent->parent->left) {
+			y = n->parent->parent->right;
+			if (y->color == RED) {
+				n->parent->color = BLACK;
+				y->color = BLACK;
+				n->parent->parent->color = RED;
+				n = n->parent->parent;
+			}
+			else if (n == n->parent->right) {
+				n = n->parent;
+				rb_leftrotation(tree,n);
+			}else{
+				n->parent->color = BLACK;
+				n->parent->parent->color = RED;
+				rb_rightrotation(tree, n->parent->parent);
+			}
+
+		}
+		else {
+			y = n->parent->parent->right;
+			if (y->color == RED) {
+				n->parent->color = BLACK;
+				y->color = BLACK;
+				n->parent->parent->color = RED;
+				n = n->parent->parent;
+			}
+			else if (n == n->parent->left) {
+				n = n->parent;
+				rb_rightrotation(tree, n);
+			}
+			else {
+				n->parent->color = BLACK;
+				n->parent->parent->color = RED;
+				rb_leftrotation(tree, n->parent->parent);
+			}
+		}
+	}
+	tree->color = BLACK;*/
+	/*printf("11");
+	RBNodePtr rbparent = NULL;
+	RBNodePtr rbgparent = NULL;
+	printf("111");
+	while ((n != tree) && (n->color != BLACK) && (n->parent->color == RED)) {
+		rbparent = n->parent;
+		rbgparent = n->parent->parent;
+		if (rbparent == rbgparent->left) {
+			RBNodePtr rbuncle = rbgparent->right;
+			if (rbuncle != NULL && rbuncle->color == RED) {
+				rbgparent->color = RED;
+				rbparent->color = BLACK;
+				rbuncle->color = BLACK;
+				n = rbgparent;
+				printf("22");
+			}
+			else {
+				if (n == rbparent->right) {
+					rb_leftrotation(tree,rbparent);
+					n = rbparent;
+					rbparent = n->parent;
+					printf("33");
 				}
-				leftrotation(tree, gparent);
-				int tmp = parent->color;
-				parent->color = gparent->color;
-				gparent->color = tmp;
-				ttree = parent;
+				rb_rightrotation(tree,rbgparent);
+				int tmp = rbparent->color;
+				rbparent->color = rbgparent->color;
+				rbgparent->color = tmp;
+				n = rbparent;
+				printf("44");
+			}
+		}
+		else {
+			RBNodePtr rbuncle = rbgparent->right;
+			if ((rbuncle != NULL) && (rbuncle->color == RED)) {
+				rbgparent->color = RED;
+				rbparent->color = BLACK;
+				rbuncle->color = BLACK;
+				n = rbgparent;
+				printf("55");
+			}
+			else {
+				if (n == rbparent->left) {
+					rb_rightrotation(tree,rbparent);
+					n = rbparent;
+					rbparent = n->parent;
+					printf("66");
+				}
+				rb_leftrotation(tree,rbgparent);
+				int tmp = rbparent->color;
+				rbparent->color = rbgparent->color;
+				rbgparent->color = tmp;
+				printf("77");
 			}
 		}
 	}
 	tree->color = BLACK;
+	printf("88");
 }
-void rbinsert(int newval) {
-	rbnode *newtree = new rbnode(newval);
-	rbnode *newroot=  NULL;
 
-	newroot = bstinsert(newroot, newtree);
-	rbfixup(newroot, newtree);
-}
-/*void rbprint(rbtree *&root, rbnode *&tree, int level) {
-	if (tree->right == NULL)
-		rbprint(root, tree->right, level + 1);
-	for (int i = 0; i < level; i++) 
-		printf(" ");
-	printf("%d [%s]\n", tree->val, tree->color);
-	if (tree->left == NULL)
-		rbprint(root, tree->left, level + 1);
-	printf("뿌엑");
-}메인에서 tree해결안됨*/
-void rbprint( rbtree * root, rbnode *&tree, int level) {
-	if (tree->right == NULL)
-		rbprint( root,tree->right, level + 1);
+void rb_print(BSTPtr self, RBNodePtr tree, int level) {
+	if (tree->right != NULL)
+		rb_print(self, tree->right, level + 1);
 	for (int i = 0; i < level; i++)
-		printf(" ");
-	printf("%d [%s]\n", tree->val, tree->color);
-	if (tree->left == NULL)
-		rbprint(root, tree->left, level + 1);
-	printf("뿌엑");
+		printf("    ");
+	printf("%d[%d]\n", tree->val, tree->color);
+	if (tree->left != NULL)
+		rb_print(self, tree->left, level + 1);
 }
 
-int main(void) {
-	printf("ff");
-	RBtree tree;
-	printf("gg");
-	rbinsert(1);
-	rbinsert(2);
-	rbinsert(7); 
-	rbinsert(5); 
-	rbinsert(4); 
-	rbinsert(8); 
-	rbinsert(15);
-	rbinsert(11);
-	rbinsert(14);
-	printf("hh");
-	inorderhelper((&tree)->root);
-	printf("jj");
-	rbprint((&tree),(&tree)->root, 0);
+void rb_inorder(BSTPtr self, RBNodePtr tree) {
+	if (tree == NULL)
+		return;
+	else {
+		rb_inorder(self, tree->left);
+		printf("%d ", tree->val);
+		rb_inorder(self, tree->right);
+	}
 }
+
+/*void main() {
+	printf("aa");
+	BSTPtr bst = bst_alloc();
+	printf("bb");
+	rb_insert(bst, bst->root, rbnode_alloc(11));
+	rb_insert(bst, bst->root, rbnode_alloc(2));
+	rb_insert(bst, bst->root, rbnode_alloc(1));
+	rb_insert(bst, bst->root, rbnode_alloc(7));
+	rb_insert(bst, bst->root, rbnode_alloc(5));
+	rb_insert(bst, bst->root, rbnode_alloc(4));
+	rb_insert(bst, bst->root, rbnode_alloc(14));
+	rb_insert(bst, bst->root, rbnode_alloc(8));
+	rb_insert(bst, bst->root, rbnode_alloc(15));
+	printf("cc");
+	rb_print(bst, bst->root, 0);
+	printf("\n");
+	rb_inorder(bst, bst->root);
+	printf("\n");
+	printf("\n[0]-BLACK [1]-RED\n");
+	printf("\n");
+}*/
 
 /*void rb_insert(rbroot* self, rbnode* tree, rbnode* n) {
 	if (self->root == NULL)
